70e58977c0ae2e77f43402ff44ba2516b24aeb26
wsulansari <118707080+wsulansari@users.noreply.github.com>
2022-12-02 10:25:14 +0700

    Update kalku.js

--- kalku.js
+++ kalku.js
@@ -6,10 +6,20 @@ if(Nilai1 == 1){
     var MasukanNilai1 = readline.questionInt('Masukan Nilai 1:')
     var MasukanNilai2 = readline.questionInt('Masukan Nilai 2:')
     var Nilai1 = MasukanNilai1 + MasukanNilai2
-    console.log(Number(MasukanNilai1)+(Number(MasukanNilai2)))
+    console.log(Nilai1)
 }else if(Nilai1 == 2){
     var MasukanNilai1 = readline.questionInt('Masukan Nilai 1:')
     var MasukanNilai2 = readline.questionInt('Masukan Nilai 2:')
     var Nilai1 = MasukanNilai1 - MasukanNilai2
-    console.log(Number(MasukanNilai1)-(Number(MasukanNilai2)))
+    console.log(Nilai1)
+}else if(Nilai1 == 3){
+    var MasukanNilai1 = readline.questionInt('MASUKAN NILAI 1:')
+    var MasukanNilai2 = readline.questionInt('MASUKAN NILAI 2:')
+    var Nilai1 = MasukanNilai1 * MasukanNilai2
+    console.log(Nilai1)
+}else if(Nilai1 == 4){
+    var MasukanNilai1 = readline.questionInt('MASUKAN NILAI 1:')
+    var MasukanNilai2 = readline.questionInt('MASUKAN NILAI 2:')
+    var Nilai1 = MasukanNilai1 / MasukanNilai2
+    console.log(Nilai1)
 }
